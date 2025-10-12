ca8945f0e374cec4e4ea9d6bfd139471f0df9b65
ericmegalovania <1486853661@qq.com>
2025-10-12 13:55:16 +0800

    revise default source, add local debugger

---
+++ local/README.txt
@@ -0,0 +1,3 @@
+put this folder under somewhere like "mingw64\lib\gcc\x86_64-w64-mingw32\11.5.0\include\c++"
+
+dbg.h from "https://github.com/sharkdp/dbg-macro"
---
+++ local/test.h
@@ -0,0 +1,18 @@
+#ifndef __LOCAL_TEST
+#define __LOCAL_TEST
+#define s S.c_str()
+char TEST_GEN[1000],TEST_ANS[1000],TEST_OUT[1000],TEST_FC[1000];
+void TEST(string S,string sol="sol"){
+	sprintf(TEST_GEN,"%s_gen.exe > %s_input.txt",s,s);
+	sprintf(TEST_ANS,"%s_%s.exe < %s_input.txt > %s_ans.txt",s,sol.c_str(),s,s);
+	sprintf(TEST_OUT,"%s.exe < %s_input.txt > %s_out.txt",s,s,s);
+	sprintf(TEST_FC,"fc %s_ans.txt %s_out.txt /n",s,s);
+	while(1){
+		cerr<<"gen"<<endl; system(TEST_GEN);
+		cerr<<"ans"<<endl; system(TEST_ANS);
+		cerr<<"out"<<endl; system(TEST_OUT);
+		if(system(TEST_FC)) break;
+	}
+}
+#undef s
+#endif
