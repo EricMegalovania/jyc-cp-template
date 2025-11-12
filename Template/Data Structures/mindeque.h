526fb1e90b13c8a19f1c884baf4f6e7f5c9b6566
ericmegalovania <1486853661@qq.com>
2025-11-12 12:32:24 +0800

    update mindeque

--- Template/Data Structures/mindeque.h
+++ Template/Data Structures/mindeque.h
@@ -1,42 +1,42 @@
 // mindeque template from 
 // https://codeforces.com/blog/entry/122003
-struct minstack {
-	stack<pair<int, int>> st; //{T1, T2}
-	int getmin() {return st.top().second;} //T2
-	bool empty() {return st.empty();}
-	int size() {return st.size();}
-	void push(int x) { //x is T1
-		int mn = x; //mn is T2
-		if (!empty()) mn = min(mn, getmin());
-		st.push({x, mn});
+template<class S,auto op>struct minstack{
+	stack<pair<S,S>>st; // {T1, T2}
+	S getmin(){ return st.top().second; } // T2
+	bool empty(){ return st.empty(); }
+	int size(){ return (int)st.size(); }
+	void push(S x){ // x is T1
+		S mn=x; // mn is T2
+		if(!empty()) mn=op(mn,getmin());
+		st.emplace(x,mn);
 	}
-	void pop() {st.pop();}
-	int top() {return st.top().first;} //T1
-	void swap(minstack &x) {st.swap(x.st);}
+	void pop(){ st.pop(); }
+	S top(){ return st.top().first; } // T1
+	void swap(minstack &x){ st.swap(x.st); }
 };
-struct mindeque {
-	minstack l, r, t;
-	void reb() { //rebalance
-		bool f = false;
-		if (r.empty()) {f = true; l.swap(r);}
-		int sz = r.size() / 2;
-		while (sz--) {t.push(r.top()); r.pop();}
-		while (!r.empty()) {l.push(r.top()); r.pop();}
-		while (!t.empty()) {r.push(t.top()); t.pop();}
-		if (f) l.swap(r);
+template<class S,auto op>struct mindeque{
+	minstack<S,op>l,r,t;
+	void reb(){ // rebalance
+		bool f=0;
+		if(r.empty()){ f=1; l.swap(r); }
+		int sz=(int)(r.size())/2;
+		while(sz--){ t.push(r.top()); r.pop(); }
+		while(!r.empty()){ l.push(r.top()); r.pop(); }
+		while(!t.empty()){ r.push(t.top()); t.pop(); }
+		if(f) l.swap(r);
 	}
-	int getmin() { //T2
-		if (l.empty()) return r.getmin();
-		if (r.empty()) return l.getmin();
-		return min(l.getmin(), r.getmin());
+	S getmin(){ // T2
+		if(l.empty()) return r.getmin();
+		if(r.empty()) return l.getmin();
+		return op(l.getmin(), r.getmin());
 	}
-	bool empty() {return l.empty() && r.empty();}
-	int size() {return l.size() + r.size();}
-	void push_front(int x) {l.push(x);} //arg is T1
-	void push_back(int x) {r.push(x);} //arg is T1
-	void pop_front() {if (l.empty()) reb(); l.pop();}
-	void pop_back() {if (r.empty()) reb(); r.pop();}
-	int front() {if (l.empty()) reb(); return l.top();} //T1
-	int back() {if (r.empty()) reb(); return r.top();} //T1
-	void swap(mindeque &x) {l.swap(x.l); r.swap(x.r);}
+	bool empty(){ return l.empty() && r.empty(); }
+	int size(){ return (int)l.size()+(int)r.size(); }
+	void push_front(S x){ l.push(x); } // arg is T1
+	void push_back(S x){ r.push(x); } // arg is T1
+	void pop_front(){ if(l.empty()) reb(); l.pop(); }
+	void pop_back(){ if(r.empty()) reb(); r.pop(); }
+	S front(){ if(l.empty()) reb(); return l.top(); } // T1
+	S back(){ if(r.empty()) reb(); return r.top(); } // T1
+	void swap(mindeque &x){ l.swap(x.l); r.swap(x.r); }
 };
