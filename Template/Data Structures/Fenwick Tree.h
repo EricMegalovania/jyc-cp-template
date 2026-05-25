75b55724bf357420e4f18976414aee3162561328
ericmegalovania <1486853661@qq.com>
2026-05-25 12:40:17 +0800

    add e() to Fenwick

--- Template/Data Structures/Fenwick Tree.h
+++ Template/Data Structures/Fenwick Tree.h
@@ -1,6 +1,6 @@
 template<typename T>
-inline T __fen_add(const T x, const T y){ return x + y; }
-template<typename T,auto op=__fen_add<T>> // T is int or LL
+inline T __fen_add(const T x, const T y){ return x+y; }
+template<typename T,auto op=__fen_add<T>,auto e=[](){ return T(0); }> // T is int or LL
 struct Fenwick{
 	int n,LOG;
 	vector<T>c;
@@ -12,23 +12,23 @@ struct Fenwick{
 		init(n_);
 	}
 	void init(int n_){
-		c.assign(n=n_,T(0));
+		c.assign(n=n_,e());
 		LOG=bit_width((unsigned)n);
 	}
 	void add(int i,T x){
 		for(;i<n;i+=lowbit(i)) c[i]=op(c[i],x);
 	}
 	T query(int i){
-		T res=0;
+		T res=e();
 		for(;i;i-=lowbit(i)) res=op(res,c[i]);
 		return res;
 	}
 	int kth(T k){ // min p that sum(1,p)>=k
-		int ans=0; T cnt=0;
+		int ans=0; T res=e();
 		for(int i=LOG;~i;--i) {
 			int nxt=ans+(1<<i);
-			if(nxt<n && op(cnt,c[nxt])<k){
-				cnt=op(cnt,c[nxt]);
+			if(nxt<n && op(res,c[nxt])<k){
+				res=op(res,c[nxt]);
 				ans=nxt;
 			}
 		}
